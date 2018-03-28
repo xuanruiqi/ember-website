18e9365c93aa9a86b3af688faed2e5e57ac41c80
Xuanrui Qi <xuanruiqi@users.noreply.github.com>
2018-03-28 03:02:34 -0400

    Added BibTeX popups pretty fancy

--- app/routes/research.js
+++ app/routes/research.js
@@ -1,4 +1,33 @@
 import Route from '@ember/routing/route';
+import {htmlSafe} from '@ember/string';
+
+function formatBibtexBlock(str) {
+    return htmlSafe('<pre>\n' + str + '\n</pre>');
+}
+
+let uglyBibtex = 
+{
+SHT2018:
+`
+@misc{QiSeniorThesis2018,
+  author = {Qi, Xuanrui (Ray)},
+  title = {Memory Tracing for Object-Oriented and Functional Programming},
+  year = {2018},
+  note = {Senior honors thesis, Tufts University},
+}
+`
+}
 
 export default Route.extend({
+    model() {
+        return [
+            {
+                abbrev: "Senior Honors Thesis 2018 (in progress)",
+                citation: "Xuanrui (Ray) Qi. 2018. Garbage Collection Tracing for Object-Oriented and Functional \
+                Programming. Senior Honors Thesis, Tufts University.",
+                bibtex: formatBibtexBlock(uglyBibtex["SHT2018"]),
+                notes: "Committee: Sam Guyer (chair), Kathleen Fisher"
+            }
+        ];
+    }
 });
